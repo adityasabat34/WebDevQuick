acc3ca23f0ebfe9e4fcf2edd6782a56d622d38c3
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-01-08 19:07:32 +0530

    DOM manipulation

--- Two/script.js
+++ Two/script.js
@@ -13,17 +13,32 @@
 //   h1.style.textAlign = "center";
 // });
 
-let div = document.querySelector("div");
-div.addEventListener("click", function () {
-  div.style.backgroundColor = "purple";
+// let div = document.querySelector("div");
+// div.addEventListener("click", function () {
+//   div.style.backgroundColor = "purple";
 
-  div.style.textAlign = "center";
-  div.style.color = "yellow";
-});
+//   div.style.textAlign = "center";
+//   div.style.color = "yellow";
 
-let inner = document.querySelector(".inner");
+// });
 
-inner.addEventListener("click", (e) => {
-  e.stopPropagation();
-  inner.style.backgroundColor = "royalblue";
+// let inner = document.querySelector(".inner");
+
+// inner.addEventListener("click", (e) => {
+//   e.stopPropagation();
+//   inner.style.backgroundColor = "royalblue";
+// });
+
+const bulb = document.querySelector(".bulb");
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", () => {
+  // Check current text inside the button
+  if (btn.innerHTML.trim() === "off") {
+    bulb.style.backgroundColor = "yellow";
+    btn.innerHTML = "on"; // Set the button text to "on"
+  } else {
+    bulb.style.backgroundColor = "black";
+    btn.innerHTML = "off"; // Set the button text to "off"
+  }
 });
